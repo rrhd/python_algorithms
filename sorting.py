f4d279063c684792269a3b6338a1fd75701967c9
Ron Heichman <ron.heichman@fullswinggolf.com>
2020-06-07 22:08:15 -0400

    Adds quick sort along with graphing of true and theoretical performance.

--- sorting.py
+++ sorting.py
@@ -50,6 +50,31 @@ class Sort():
             sorted_list = self.bubble_sort_func2(a_list)
         return {'func_call_count': self.func_call_count, 'sorted_list': sorted_list}
 
+    def quick_sort_func(self, a_list, first=None, last=None):
+            if first is None:
+                first = 0
+                last = len(a_list) - 1
+            if first < last:
+                pivot = self.partition(a_list, first, last)
+
+                self.quick_sort_func(a_list, first, pivot - 1)
+                self.quick_sort_func(a_list, pivot + 1, last)
+
+    def partition(self, a_list, first, last):
+        pivot = a_list[last]
+        i = first - 1
+        for j in range(first, last):
+            if self.num_comp(a_list[j], pivot):
+                i += 1
+                a_list[j], a_list[i] = self.num_switch(a_list[j], a_list[i])
+        a_list[last], a_list[i + 1] = self.num_switch(a_list[last], a_list[i + 1])
+        return i + 1
+
+    def quick_sort(self, a_list):
+        self.func_call_count = 0
+        self.quick_sort_func(a_list)
+        return {'func_call_count': self.func_call_count, 'sorted_list': a_list}
+
     def bubble_sort_func2(self, a_list):
         for i in range(len(a_list) - 1, 0, -1):
             for j in range(i):
@@ -78,26 +103,43 @@ class Sort():
         self.func_call_count += 1
         return b, a
 
-n = 100
-m = 100
-func_call_count_array = np.zeros((m * n, 2))
+
 sorter = Sort()
-j = 0
-for i in range(n):
-    k = 0
-    while (k < m):
-        sorted_list = sorter.merge_sort([random.randint(0, k) for k in range(i)])
-        func_call_count_array[j, 0] = i
-        func_call_count_array[j, 1] = sorted_list['func_call_count']
-        j += 1
-        k += 1
-x = func_call_count_array[:, 0]
-y_exp = (func_call_count_array[:, 1])
-y_true = x[x != 0] * np.log(x[x != 0])
-# y_true = func_call_count_array[:,0] ** 2
-C = y_exp[x != 0][y_true != 0] / y_true[y_true != 0]
-C = C[~(np.isnan(C) | np.isinf(C))]
-C = C.mean()
-plt.scatter(x, y_exp)
-plt.scatter(x[x != 0], C * y_true)
-plt.show()
+# items = [0, 1, 0, 0, 2, 4, 7, 7, 4, 10, 7, 11, 3, 12, 14, 6, 6, 13, 10]
+# items = [10, 80, 30, 90, 40, 50, 70]
+# sorter.quick_sort(items)
+#
+# n = 100
+# m = 100
+# func_call_count_array = np.zeros((m * n, 2))
+# func_call_count_array_2 = np.zeros((m * n, 2))
+# sorter = Sort()
+# j = 0
+# for i in range(n):
+#     k = 0
+#     while (k < m):
+#         items = [random.randint(0, k) for k in range(i)]
+#         sorted_list = sorter.quick_sort(items)
+#         func_call_count_array[j, 0] = i
+#         func_call_count_array[j, 1] = sorted_list['func_call_count']
+#         sorted_list = sorter.quick_sort(items)
+#         func_call_count_array_2[j, 0] = i
+#         func_call_count_array_2[j, 1] = sorted_list['func_call_count']
+#         j += 1
+#         k += 1
+# x = func_call_count_array[:, 0]
+# y_exp = (func_call_count_array[:, 1])
+# y_exp_2 = (func_call_count_array_2[:, 1])
+# y_true = x[x != 0] * np.log(x[x != 0])
+# # y_true = func_call_count_array[:,0] ** 2
+# C = y_exp[x != 0][y_true != 0] / y_true[y_true != 0]
+# C = C[~(np.isnan(C) | np.isinf(C))]
+# C = C.mean()
+# C2 = y_exp_2[x != 0][y_true != 0] / y_true[y_true != 0]
+# C2 = C2[~(np.isnan(C2) | np.isinf(C2))]
+# C2 = C2.mean()
+# # plt.scatter(x, y_exp)
+# plt.scatter(x, y_exp_2)
+# # plt.scatter(x[x != 0], C * y_true)
+# plt.scatter(x[x != 0], C2 * y_true)
+# plt.show()
